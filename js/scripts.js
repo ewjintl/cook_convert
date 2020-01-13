d46ac964a3bfcebb879307c8bcf669650f5d7d4b
ewjintl@icloud.com and nickgallo41@gmail.com <example@example.com>
2020-01-13 15:19:56 -0800

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+var convertOunce = function(numberOfOunce) {
+return numberOfOunce * 28.3495
+}
+
+var convertOunceReturn = parseInt(prompt("Enter number of ounce:"));
+var result = convertOunce(convertOunceReturn);
+alert(result)
+
+// var number2 = parseInt(prompt("Enter another number:"));
+
+// var gallon = 1
+// var liter = 3.8
+
+// var ounce = 1
+// var gram = 28
+
+// var pound = 1
+// var kilo = 455 
